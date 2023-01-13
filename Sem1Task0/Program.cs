699809ad856d6182bc184adf7b5801d8ab47ac8c
anuta.v <anuta-v@yandex.ru>
2023-01-13 22:10:41 +0300

    add solution for task 0,1,3,5,7

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача №0
+// Напишите программц, которая на вход принимает
+// число и выдает его квадрат (число умноженное
+// на себя
+
+// Считываем данные с консоли
+
+
+String? inputNum = Console.ReadLine();
+if(inputNum!=null)
+
+{
+  
+    int number = int.Parse(inputNum);
+
+    int result = number*number;   
+
+    Console.WriteLine(result);
+
+}
+
