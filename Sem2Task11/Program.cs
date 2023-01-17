cafa540be49619997050c39c0a714782202488bc
anuta.v <anuta-v@yandex.ru>
2023-01-17 22:28:06 +0300

    add solution for task 9.11.12.14.16

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №11
+// Напишите программу, которая выводит случайное трёхзначное число и 
+// удаляет вторую цифру этого числа.
+
+
+System.Random newSintezator = new System.Random();
+int num = newSintezator.Next(100,1000);
+int digitfirst = num/100;
+int digitLast = num%10;
+Console.WriteLine(num);
+Console.WriteLine(digitfirst*10 + digitLast);
