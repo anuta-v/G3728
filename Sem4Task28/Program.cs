4d4af12b49e28509a7cd2909e4a7285496e18478
anuta.v <anuta-v@yandex.ru>
2023-01-24 22:27:50 +0300

    Add Task 24.26.28.30

---
+++ Sem4Task28/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача №28
+// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+
+using System.Numerics;
+
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// выводим результат пользователя
+void PrintData(string msg1, BigInteger msg2)
+{
+    Console.WriteLine(msg1);
+    Console.WriteLine(msg2);
+}
+
+BigInteger CalcFact(int num)
+{
+    BigInteger res = 1;
+    for (int i= 1; i < num; i++)
+    {
+        res = res * i;
+    }
+    return res;
+}
+
+int number = ReadData("Ведите число");
+
+BigInteger fact = CalcFact(number);
+
+PrintData("Факториал равен: ", fact);
